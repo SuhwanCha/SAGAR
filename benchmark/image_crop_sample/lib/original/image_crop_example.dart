06eb342c4baaa2c44758fbbc5949f9b4a773b8ff
SuhwanCha <suhwan.cha@ezar.co.kr>
2024-06-13 22:44:00 +0900

    t

--- benchmark/image_crop_sample/lib/original/image_crop_example.dart
+++ benchmark/image_crop_sample/lib/original/image_crop_example.dart
@@ -6,18 +6,18 @@ class ImageCropper {
   Future<File> crop(File original) async {
     final sagar = Sagar<File>();
     return sagar.execute(() async {
-      final image = decodeImage(original.readAsBytesSync())!;
-      final cropped = copyCrop(image,
-          x: 0,
-          y: 0,
-          width: image.width,
-          height: image.height,
-          radius: 0,
-          antialias: true);
-      final String name = original.path.split(RegExp(r'(/|\\)')).last;
-      final croppedFile = File('${original.parent.path}/cropped4-$name');
-      await croppedFile.writeAsBytes(encodePng(cropped));
-      return croppedFile;
+    final image = decodeImage(original.readAsBytesSync())!;
+    final cropped = copyCrop(image,
+        x: 0,
+        y: 0,
+        width: image.width,
+        height: image.height,
+        radius: 0,
+        antialias: true);
+    final String name = original.path.split(RegExp(r'(/|\\)')).last;
+    final croppedFile = File('${original.parent.path}/cropped4-$name');
+    await croppedFile.writeAsBytes(encodePng(cropped));
+    return croppedFile;
     });
   }
 }
