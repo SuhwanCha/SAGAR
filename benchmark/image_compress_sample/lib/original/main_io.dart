5e2a5463a9e656c41b4b9e393c185094c78cb8ea
SuhwanCha <suhwan.cha@ezar.co.kr>
2024-06-13 22:23:22 +0900

    t

--- benchmark/image_compress_sample/lib/original/main_io.dart
+++ benchmark/image_compress_sample/lib/original/main_io.dart
@@ -1,18 +1,31 @@
 // ignore_for_file: deprecated_member_use
 
 import 'dart:async';
-import 'dart:isolate';
-
+import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
-
 import 'package:flutter/material.dart' hide TextButton;
 import 'package:flutter/services.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:sagar/sagar.dart';
 
 Future<void> runMain() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
   FlutterImageCompress.showNativeLog = true;
+}
+
+class Sagar extends SagarBase<Uint8List> {
+  @override
+  Future<Uint8List> execute() async {
+    const img = AssetImage('img/img.jpg');
+    const config = ImageConfiguration();
+    final AssetBundleImageKey key = await img.obtainKey(config);
+    final ByteData data = await key.bundle.load(key.name);
+    final result = await FlutterImageCompress.compressWithList(
+      data.buffer.asUint8List(),
+    );
+    return result;
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -22,66 +35,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool _isLoading = false;
-  ImageProvider? provider;
-  Future<Uint8List> compress() async {
-    const img = AssetImage('img/img.jpg');
-    const config = ImageConfiguration();
-    final AssetBundleImageKey key = await img.obtainKey(config);
-    final ByteData data = await key.bundle.load(key.name);
-    final receivePort = ReceivePort();
-    final isolate = await Isolate.spawn<_IsolateMessage>(
-      _isolateEntryPoint,
-      _IsolateMessage(receivePort.sendPort, data.buffer.asUint8List()),
-    );
-    final result = await receivePort.first;
-    receivePort.close();
-    isolate.kill(priority: Isolate.immediate);
-    return result;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Image Compress Sample'),
-      ),
-      body: Center(
-        child: _isLoading
-            ? const CircularProgressIndicator()
-            : Column(
+    final sagar = Sagar();
+    return SagarProvider.value(
+      value: sagar,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Image Compress Sample'),
+        ),
+        body: SagarBuilder<Uint8List, Sagar>(
+          builder: (context, value) {
+            return Center(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   ElevatedButton(
-                    onPressed: () async {
-                      setState(() {
-                        _isLoading = true;
-                      });
-                      final result = await compress();
-                      setState(() {
-                        provider = MemoryImage(result);
-                      });
-                    },
+                    onPressed: () => sagar.init(),
                     child: const Text('Compress Image'),
                   ),
-                  if (provider != null) Image(image: provider!),
+                  Image(image: MemoryImage(value)),
                 ],
               ),
+            );
+          },
+          errorBuilder: (context) {
+            return const Center(
+              child: Text('Error'),
+            );
+          },
+          loadingBuilder: (context) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
       ),
     );
   }
-}
-
-void _isolateEntryPoint(_IsolateMessage message) {
-  final sendPort = message.sendPort;
-  final result = FlutterImageCompress.compressWithList(
-    message.model,
-  );
-  sendPort.send(result);
-}
-
-class _IsolateMessage {
-  final SendPort sendPort;
-  final Uint8List model;
-  _IsolateMessage(this.sendPort, this.model);
 }
