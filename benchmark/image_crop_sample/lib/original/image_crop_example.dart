a5e5406cb3dfb082a3abd12cb53e8927b026cc0d
SuhwanCha <suhwan.cha@ezar.co.kr>
2024-06-13 22:31:54 +0900

    t

--- benchmark/image_crop_sample/lib/original/image_crop_example.dart
+++ benchmark/image_crop_sample/lib/original/image_crop_example.dart
@@ -1,10 +1,22 @@
 import 'dart:io';
+import 'dart:isolate';
 import 'package:image/image.dart';
 
 class ImageCropper {
+  Future<File> cropIsolate(File original) async {
+    final receivePort = ReceivePort();
+    final isolate = await Isolate.spawn<IsolatePayload>(
+      _isolateEntryPoint,
+      IsolatePayload(original, receivePort.sendPort),
+    );
+    final croppedFile = await receivePort.first;
+    receivePort.close();
+    isolate.kill(priority: Isolate.immediate);
+    return croppedFile;
+  }
+
   static Future<File> crop(File original) async {
     final image = decodeImage(original.readAsBytesSync())!;
-
     final cropped = copyCrop(image,
         x: 0,
         y: 0,
@@ -12,13 +24,21 @@ class ImageCropper {
         height: image.height,
         radius: 0,
         antialias: true);
-
     final String name = original.path.split(RegExp(r'(/|\\)')).last;
-
     final croppedFile = File('${original.parent.path}/cropped4-$name');
-
     await croppedFile.writeAsBytes(encodePng(cropped));
-
     return croppedFile;
   }
+}
+
+void _isolateEntryPoint(IsolatePayload message) {
+  ImageCropper.crop(message.original).then((croppedFile) {
+    message.sendPort.send(croppedFile);
+  });
+}
+
+class IsolatePayload {
+  final File original;
+  final SendPort sendPort;
+  IsolatePayload(this.original, this.sendPort);
 }
